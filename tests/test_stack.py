1f81db79d8582c8947129e124cd7ea0513a6a5de
Mihail Mihaylov <chakala1975@gmail.com>
2025-09-22 21:35:53 +0300

    Update stack tests

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -3,26 +3,23 @@ import pytest
 from src.algorithms.structures.stack import Stack
 
 
-def test_push_then_pop_returns_last_pushed() -> None:
-    st = Stack[int]()
-    st.push(1)
-    assert st.pop() == 1
+def test_push_then_pop_returns_last_pushed(empty_stack: Stack[int]) -> None:
+    empty_stack.push(1)
+    assert empty_stack.pop() == 1
 
 
-def test_peek_returns_top_without_removal() -> None:
-    st = Stack[int]()
-    st.push(10)
-    assert st.peek() == 10
+def test_peek_returns_top_without_removal(empty_stack: Stack[int]) -> None:
+    empty_stack.push(10)
+    assert empty_stack.peek() == 10
 
 
-def test_size_after_multiple_pushes() -> None:
-    st = Stack[int]()
-    st.push(1)
-    st.push(2)
-    assert st.size() == 2
+def test_size_after_multiple_pushes(empty_stack: Stack[int]) -> None:
+    empty_stack.push(1)
+    empty_stack.push(2)
+    assert empty_stack.size() == 2
 
 
-def test_pop_on_empty_raises() -> None:
-    st = Stack[int]()
+def test_pop_on_empty_raises(empty_stack: Stack[int]) -> None:
     with pytest.raises(IndexError):
-        st.pop()
+        empty_stack.pop()
+
